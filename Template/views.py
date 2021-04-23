73df875418e68c93793f473f685fd80e281c1309
Mario <mario.tapia@alumnos.upla.com>
2021-04-22 22:24:08 -0400

    Optimied code

--- Template/views.py
+++ Template/views.py
@@ -5,112 +5,96 @@ from django.db import IntegrityError
 import time
 import requests                                                  
 import json
+import aiohttp
+import asyncio
+from time import perf_counter
 
 # Create your views here.
-def home(request):    
+def home(request):  
     dataStats = []
-    viewg = tiposGroups(request)
-    cantusu = cantUsuarios(requests)
-    ctranc = cantTransacc(request)
-    print(viewg[1])
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    dataCall = asyncio.run(cyclosApiCall())
+    tiposGroups(dataCall[0])
+    listaGroups = listaGrupos()
+    cantgroups = cantGrupos()
+    cantusu = cantUsuarios(dataCall[1])
+    ctranc = cantTransacc(dataCall[2])
+    
     dataStats.extend([cantusu,
-                         viewg[1],
+                         cantgroups,
                          int(ctranc[0]),
                          float(ctranc[1])])
     saveData(dataStats)
-   
-    return render(request, "paginas/home.html",{'viewgrupos':viewg[0], 
-                                                'cantg':viewg[1], 
-                                                'cantusu':cantusu, 
-                                                'canttr':ctranc[0], 
-                                                'sumtr':ctranc[1]})
+    
+    dinamicStats = data.objects.order_by('fecha_add').last()
+    
+    return render(request, "paginas/home.html",{'viewgrupos':listaGroups, 
+                                                'cantg':dinamicStats.cant_nodos, 
+                                                'cantusu':dinamicStats.cant_usuarios, 
+                                                'canttr':dinamicStats.cant_transacc, 
+                                                'sumtr':dinamicStats.sum_valpos})
     
 def reglamento(request):
     
     return render(request, "paginas/reglamento.html")
 
 
-def tiposGroups(request):
-    #obtiene la lista de la Api se transforma en objeto de json y se extrae los datos necesarios
+def tiposGroups(grupos):
+    objGrupos =  [] 
+    resultObject = grupos['groups']
+    #separo y obtengo solo los grupos tipo memeberGroup
+    for tipogroup in resultObject: 
+        if tipogroup['kind'] == 'memberGroup' and tipogroup['groupSet'] == 'nodoValpos' :
+            objGrupos.append(tipogroup)     
+            
+    #Busco en la BD si la id del grupo esta agregada, si no esta se agrega
+            
+    for ingrupos in objGrupos:
+        #Aqui si el objeto no existe, se devuelve una tupla donde el objeto se guarda en insert
+        # y un boolean se guarda en creado, True si fue creado el objeto. 
+        try:
+            insert, creado = Comunidad.objects.get_or_create(cod_comunidad = ingrupos['id'],
+                                        nom_comunidad = ingrupos['name'], 
+                                        descrip = "Activar y Agregar Descripción",
+                                        hubi_comuni = 'Activar',
+                                        coor_comuni='0')
+            if creado:                     
+                insert.save() 
+        except IntegrityError:
+            #Si hay un cambio de nombre que no provenga desde cyclos, habra un error de integridad
+            # y entrara aqui, aca obtendre el qreryset con la id del del campo cod_comunidad.
+            #luego comparare el nombre del grupo de la BD de esa fila con el nombre del grupo que traigo desde cyclos y se cambia
+            #por el de cyclos.
+            cambionom = Comunidad.objects.get(cod_comunidad = ingrupos['id'])
+            #print(cambionom.nom_comunidad)
+            if cambionom.nom_comunidad != ingrupos['name']:
+                cambionom.nom_comunidad = ingrupos['name']
+                cambionom.save()
 
-    auth = Auth()
-    grupos =  []
-    viewg = []
+def listaGrupos():
+    listaGr = Comunidad.objects.all().values()
+    return listaGr
+
+def cantGrupos():
     countnodos = 0
-    r = requests.get('https://communities.cyclos.org/valpos/api/users/data-for-search?fields=&fromMenu=false', 
-                        auth=(auth.getUser(), 
-                        auth.getPass()))
-                        
-    if r.status_code == 200:
-        #Transformo r en un objeto de python    
-        parsedJsonObject = json.loads(r.text)
-        resultObject = parsedJsonObject['groups']
-        #separo y obtengo solo los grupos tipo memeberGroup
-        for tipogroup in resultObject: 
-            if tipogroup['kind'] == 'memberGroup' and tipogroup['groupSet'] == 'nodoValpos' :
-                grupos.append(tipogroup)     
-                
-        #Busco en la BD si la id del grupo esta agregada, si no esta se agrega
-                
-        for ingrupos in grupos:
-            #Aqui si el objeto no existe, se devuelve una tupla donde el objeto se guarda en insert
-            # y un boolean se guarda en creado, True si fue creado el objeto. 
-            try:
-                insert, creado = Comunidad.objects.get_or_create(cod_comunidad = ingrupos['id'],
-                                            nom_comunidad = ingrupos['name'], 
-                                            descrip = "Activar y Agregar Descripción",
-                                            hubi_comuni = 'Activar',
-                                            coor_comuni='0')
-                if creado:                     
-                    insert.save() 
-            except IntegrityError:
-                #Si hay un cambio de nombre que no provenga desde cyclos, habra un error de integridad
-                # y entrara aqui, aca obtendre el qreryset con la id del del campo cod_comunidad.
-                #luego comparare el nombre del grupo de la BD de esa fila con el nombre del grupo que traigo desde cyclos y se cambia
-                #por el de cyclos.
-                cambionom = Comunidad.objects.get(cod_comunidad = ingrupos['id'])
-                #print(cambionom.nom_comunidad)
-                if cambionom.nom_comunidad != ingrupos['name']:
-                    cambionom.nom_comunidad = ingrupos['name']
-                    cambionom.save()
-                                
-    else:
-        f'No se pudo hacer hacer la conexión code :{r.status_code}'
-        
-    #Grupos guardados en la lista
-    viewg.append(Comunidad.objects.all().values())
     nodos = Comunidad.objects.all().values()
 
     for elemnts in nodos:    
         if elemnts['is_active'] == True:
-            countnodos = countnodos + 1
+            countnodos += 1
+    return countnodos
 
-    viewg.append(countnodos)
-    return  viewg
-
-def cantUsuarios(requests):
-    auth = Auth()
+def cantUsuarios(cantUsers):
     countUsu = 0
-    r = requests.get('https://communities.cyclos.org/valpos/api/users?groups=nodoValpos&includeGroup=true&includeGroupSet=true&orderBy=alphabeticallyAsc&roles=member&statuses=active&pageSize=5000', 
-                        auth=(auth.getUser(), 
-                        auth.getPass()))
-    parsedJsonObject = json.loads(r.text)
- 
-    for tipoGroups in parsedJsonObject:
+    for tipoGroups in cantUsers:
         if  tipoGroups['groupSet']['internalName'] == "nodoValpos":
             countUsu=countUsu + 1
-
     return countUsu        
             
-def cantTransacc(request):
-    auth = Auth() 
+def cantTransacc(totalTransac):
     ctranc = []     
-    r = requests.get('https://communities.cyclos.org/valpos/api/transfers/summary?kinds=&skipTotalCount=false&transferKinds=', 
-                        auth=(auth.getUser(), 
-                        auth.getPass()))
-    parsedJsonObject = json.loads(r.text)
-    ctranc.append(parsedJsonObject[0]['count'])
-    ctranc.append(parsedJsonObject[0]['sum'])
+    ctranc.append(totalTransac[0]['count'])
+    ctranc.append(totalTransac[0]['sum'])
     return ctranc
 
 def saveData(dataStats):
@@ -125,3 +109,31 @@ def saveData(dataStats):
         dato.save()
     else:
         print("fecha ya ingresada")
+
+
+async def get_response(session, url):
+    auth = Auth() 
+    async with session.get(url, auth=aiohttp.BasicAuth(auth.getUser(), auth.getPass())) as resp:
+        response = await resp.json()
+        return response
+
+async def cyclosApiCall():
+
+    dataCall = []
+    async with aiohttp.ClientSession() as session:
+        urls =[
+            'https://communities.cyclos.org/valpos/api/users/data-for-search?fields=&fromMenu=false',
+            'https://communities.cyclos.org/valpos/api/users?groups=nodoValpos&includeGroup=true&includeGroupSet=true&orderBy=alphabeticallyAsc&roles=member&statuses=active&pageSize=5000',
+            'https://communities.cyclos.org/valpos/api/transfers/summary?kinds=&skipTotalCount=false&transferKinds=', 
+            ]
+
+        tasks = []
+        for url in urls:
+            tasks.append(asyncio.ensure_future(get_response(session, url)))
+
+        result_response = await asyncio.gather(*tasks)
+        for response in result_response:
+            dataCall.append(response)
+        
+        return dataCall
+

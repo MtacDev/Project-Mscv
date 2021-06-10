2ffd2d3390f3cea30a962cab7a2374d68e6a93b2
Mario Tapia <mtac.poker@gmail.com>
2021-06-10 16:36:49 -0400

    Bug fix db

--- BDmscv/admin.py
+++ BDmscv/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from BDmscv.models import Comunidad, Persona, Reporte, Modulo_Comp, AuthPago, data
+from BDmscv.models import Comunidad, Persona, Reporte, Modulo_Comp, AuthPago, data, Agradecimiento
 
 # Register your models here.
 admin.site.register(Comunidad)
@@ -8,4 +8,5 @@ admin.site.register(Reporte)
 admin.site.register(Modulo_Comp)
 admin.site.register(AuthPago)
 admin.site.register(data)
+admin.site.register(Agradecimiento)
 
